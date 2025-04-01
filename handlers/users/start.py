0449d36196e3d2431be52bf2f1881488c2c95e7b
abdufattohfattoyev <abdufattohfattoyev0@gmail.com>
2025-04-01 10:44:22 +0500

    Valyuta Qismi

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,23 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
-
+import requests
 from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"BU BOT ISHGA TUSHDIRILDI")
+    await message.answer(f"Assalomu alaykum, {message.from_user.full_name}!\n"
+                         f"Valyuta botiga xush kelibsiz!")
+
+
+@dp.message_handler(commands=['valyuta'])
+async def valyuta(message: types.Message):
+    url = 'https://cbu.uz/uz/arkhiv-kursov-valyut/json/'
+    response = requests.get(url).json()
+
+    javob = ""
+    for val in response:
+        if val['Ccy'] in ['USD', 'EUR', 'RUB']:
+            javob += f"{val['Ccy']}: {val['Rate']}\n"
+
+    await message.reply(javob if javob else "Valyuta ma'lumotlari topilmadi.")
